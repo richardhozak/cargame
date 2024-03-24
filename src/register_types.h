f893e7f554b82e5d4616f27ac6a65d3b6cb8e57f
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-03-24 23:51:53 +0100

    Apply clang-format

--- src/register_types.h
+++ src/register_types.h
@@ -8,5 +8,4 @@ using namespace godot;
 void initialize_example_module(ModuleInitializationLevel p_level);
 void uninitialize_example_module(ModuleInitializationLevel p_level);
 
-#endif // GDEXAMPLE_REGISTER_TYPES_H
-
+#endif  // GDEXAMPLE_REGISTER_TYPES_H
