0b614e72790d4be3862fbb4250807d643876451e
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-03-25 18:07:55 +0100

    Add more functionality to cpp extension

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -1,16 +1,29 @@
 #include "gdexample.h"
+
+#include <godot_cpp/classes/global_constants.hpp>
+#include <godot_cpp/classes/label.hpp>
+#include <godot_cpp/classes/material.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/node_path.hpp>
 
 using namespace godot;
 
 void GDExample::_bind_methods()
 {
+    ClassDB::bind_method(D_METHOD("get_amplitude"), &GDExample::get_amplitude);
+    ClassDB::bind_method(D_METHOD("set_amplitude", "p_amplitude"), &GDExample::set_amplitude);
+    ClassDB::add_property("GDExample", PropertyInfo(Variant::FLOAT, "amplitude"), "set_amplitude", "get_amplitude");
+
+    ClassDB::bind_method(D_METHOD("get_label"), &GDExample::get_label);
+    ClassDB::bind_method(D_METHOD("set_label", "p_label"), &GDExample::set_label);
+    ClassDB::add_property("GDExample", PropertyInfo(Variant::NODE_PATH, "label", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Label"), "set_label", "get_label");
 }
 
 GDExample::GDExample()
 {
     // Initialize any variables here.
     time_passed = 0.0;
+    amplitude = 10.0;
 }
 
 GDExample::~GDExample()
@@ -18,11 +31,40 @@ GDExample::~GDExample()
     // Add your cleanup here.
 }
 
+void GDExample::set_amplitude(const double p_amplitude)
+{
+    amplitude = p_amplitude;
+}
+
+double GDExample::get_amplitude() const
+{
+    return amplitude;
+}
+
+void GDExample::set_label(const NodePath& p_label)
+{
+    if (label_path == p_label)
+    {
+        return;
+    }
+
+    label_path = p_label;
+}
+
+NodePath GDExample::get_label() const
+{
+    return label_path;
+}
+
 void GDExample::_process(double delta)
 {
     time_passed += delta;
 
-    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+    Vector2 new_position = Vector2(
+        amplitude + (amplitude * sin(time_passed * 2.0)),
+        amplitude + (amplitude * cos(time_passed * 1.5)));
 
     set_position(new_position);
+
+    get_node<Label>(label_path)->set_text(String::num_real(time_passed));
 }
--- src/gdexample.h
+++ src/gdexample.h
@@ -1,6 +1,7 @@
 #ifndef GDEXAMPLE_H
 #define GDEXAMPLE_H
 
+#include <godot_cpp/classes/label.hpp>
 #include <godot_cpp/classes/sprite2d.hpp>
 
 namespace godot {
@@ -11,6 +12,8 @@ class GDExample : public Sprite2D
 
 private:
     double time_passed;
+    double amplitude;
+    NodePath label_path;
 
 protected:
     static void _bind_methods();
@@ -20,6 +23,12 @@ public:
     ~GDExample();
 
     void _process(double delta) override;
+
+    void set_amplitude(const double p_amplitude);
+    double get_amplitude() const;
+
+    void set_label(const NodePath& p_label);
+    NodePath get_label() const;
 };
 
 }  // namespace godot
