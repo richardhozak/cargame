e5476df19656b395d27728b0db593bf4035c0ddb
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-03-25 22:28:49 +0100

    Add node paths for wheels, body and track to gdphysics

--- src/gdphysics.cpp
+++ src/gdphysics.cpp
@@ -18,10 +18,35 @@ void GDPhysics::_bind_methods()
     ClassDB::bind_method(D_METHOD("get_label"), &GDPhysics::get_label);
     ClassDB::bind_method(D_METHOD("set_label", "p_label"), &GDPhysics::set_label);
     ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "label", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Label"), "set_label", "get_label");
+
+    ClassDB::bind_method(D_METHOD("get_wheel1"), &GDPhysics::get_wheel1);
+    ClassDB::bind_method(D_METHOD("set_wheel1", "p_wheel1"), &GDPhysics::set_wheel1);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "wheel1", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_wheel1", "get_wheel1");
+
+    ClassDB::bind_method(D_METHOD("get_wheel2"), &GDPhysics::get_wheel2);
+    ClassDB::bind_method(D_METHOD("set_wheel2", "p_wheel2"), &GDPhysics::set_wheel2);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "wheel2", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_wheel2", "get_wheel2");
+
+    ClassDB::bind_method(D_METHOD("get_wheel3"), &GDPhysics::get_wheel3);
+    ClassDB::bind_method(D_METHOD("set_wheel3", "p_wheel3"), &GDPhysics::set_wheel3);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "wheel3", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_wheel3", "get_wheel3");
+
+    ClassDB::bind_method(D_METHOD("get_wheel4"), &GDPhysics::get_wheel4);
+    ClassDB::bind_method(D_METHOD("set_wheel4", "p_wheel4"), &GDPhysics::set_wheel4);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "wheel4", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_wheel4", "get_wheel4");
+
+    ClassDB::bind_method(D_METHOD("get_body"), &GDPhysics::get_body);
+    ClassDB::bind_method(D_METHOD("set_body", "p_body"), &GDPhysics::set_body);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "body", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_body", "get_body");
+
+    ClassDB::bind_method(D_METHOD("get_track"), &GDPhysics::get_track);
+    ClassDB::bind_method(D_METHOD("set_track", "p_track"), &GDPhysics::set_track);
+    ClassDB::add_property("GDPhysics", PropertyInfo(Variant::NODE_PATH, "track", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_track", "get_track");
 }
 
 GDPhysics::GDPhysics()
 {
+    UtilityFunctions::print("ctor");
     // Initialize any variables here.
     time_passed = 0.0;
     amplitude = 10.0;
@@ -29,6 +54,7 @@ GDPhysics::GDPhysics()
 
 GDPhysics::~GDPhysics()
 {
+    UtilityFunctions::print("dtor");
     // Add your cleanup here.
 }
 
@@ -57,9 +83,20 @@ NodePath GDPhysics::get_label() const
     return label_path;
 }
 
-void GDPhysics::_ready()
+void GDPhysics::initialize()
 {
-    UtilityFunctions::print("test");
+    UtilityFunctions::print("initialize");
+}
+
+void GDPhysics::_notification(int p_what)
+{
+    switch (p_what)
+    {
+        case NOTIFICATION_READY:
+        case NOTIFICATION_EXTENSION_RELOADED:
+            initialize();
+            break;
+    }
 }
 
 void GDPhysics::_process(double delta)
@@ -79,4 +116,64 @@ void GDPhysics::_process(double delta)
             label->set_text(String::num_real(time_passed));
         }
     }
+}
+
+void GDPhysics::set_wheel1(const NodePath& p_wheel1)
+{
+    UtilityFunctions::print("set_wheel1");
+    wheel1 = p_wheel1;
+}
+NodePath GDPhysics::get_wheel1() const
+{
+    return wheel1;
+}
+
+void GDPhysics::set_wheel2(const NodePath& p_wheel2)
+{
+    UtilityFunctions::print("set_wheel2");
+    wheel2 = p_wheel2;
+}
+NodePath GDPhysics::get_wheel2() const
+{
+    return wheel2;
+}
+
+void GDPhysics::set_wheel3(const NodePath& p_wheel3)
+{
+    UtilityFunctions::print("set_wheel3");
+    wheel3 = p_wheel3;
+}
+NodePath GDPhysics::get_wheel3() const
+{
+    return wheel3;
+}
+
+void GDPhysics::set_wheel4(const NodePath& p_wheel4)
+{
+    UtilityFunctions::print("set_wheel4");
+    wheel4 = p_wheel4;
+}
+NodePath GDPhysics::get_wheel4() const
+{
+    return wheel4;
+}
+
+void GDPhysics::set_body(const NodePath& p_body)
+{
+    UtilityFunctions::print("set_body");
+    body = p_body;
+}
+NodePath GDPhysics::get_body() const
+{
+    return body;
+}
+
+void GDPhysics::set_track(const NodePath& p_track)
+{
+    UtilityFunctions::print("set_track");
+    track = p_track;
+}
+NodePath GDPhysics::get_track() const
+{
+    return track;
 }
--- src/gdphysics.h
+++ src/gdphysics.h
@@ -14,6 +14,16 @@ private:
     double time_passed;
     double amplitude;
     NodePath label_path;
+    NodePath wheel1;
+    NodePath wheel2;
+    NodePath wheel3;
+    NodePath wheel4;
+    NodePath body;
+    NodePath track;
+
+    static const int NOTIFICATION_EXTENSION_RELOADED = 2;
+
+    void initialize();
 
 protected:
     static void _bind_methods();
@@ -22,7 +32,7 @@ public:
     GDPhysics();
     ~GDPhysics();
 
-    void _ready() override;
+    void _notification(int p_what);
     void _process(double delta) override;
 
     void set_amplitude(const double p_amplitude);
@@ -30,6 +40,24 @@ public:
 
     void set_label(const NodePath& p_label);
     NodePath get_label() const;
+
+    void set_wheel1(const NodePath& p_wheel1);
+    NodePath get_wheel1() const;
+
+    void set_wheel2(const NodePath& p_wheel2);
+    NodePath get_wheel2() const;
+
+    void set_wheel3(const NodePath& p_wheel3);
+    NodePath get_wheel3() const;
+
+    void set_wheel4(const NodePath& p_wheel4);
+    NodePath get_wheel4() const;
+
+    void set_body(const NodePath& p_body);
+    NodePath get_body() const;
+
+    void set_track(const NodePath& p_track);
+    NodePath get_track() const;
 };
 
 }  // namespace godot
