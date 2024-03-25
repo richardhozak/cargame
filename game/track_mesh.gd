eb26d9d7ea7fda6db2bb57744aff4be89c79c17f
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-03-25 20:21:04 +0100

    Add first track and code to import track mesh

---
+++ game/track_mesh.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+@export var vertices: Array[PackedVector3Array]
+@export var indices: Array[PackedInt32Array]
+
+func add_mesh(vertex: PackedVector3Array, index: PackedInt32Array):
+	vertices.append(vertex)
+	indices.append(index)
