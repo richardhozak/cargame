ed3edc53fd650337c28bcaf41cbbd7f145104933
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-04-30 01:00:50 +0200

    Remove unused code

--- game/track_mesh.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-@export var vertices: Array[PackedVector3Array]
-@export var indices: Array[PackedInt32Array]
-
-
-func add_mesh(vertex: PackedVector3Array, index: PackedInt32Array):
-	vertices.append(vertex)
-	indices.append(index)
