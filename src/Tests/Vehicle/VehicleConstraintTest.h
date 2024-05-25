1b693b7bd8209fb8f1e9da0a10458da496af07fc
Richard Hozák <5235838+richardhozak@users.noreply.github.com>
2024-05-25 18:50:23 +0200

    Divide vehicle params into sections by comments

--- src/Tests/Vehicle/VehicleConstraintTest.h
+++ src/Tests/Vehicle/VehicleConstraintTest.h
@@ -49,17 +49,20 @@ private:
     static inline bool sFourWheelDrive = false;
     static inline bool sAntiRollbar = true;
     static inline bool sLimitedSlipDifferentials = true;
+    // engine
     static inline float sMaxTorque = 800.0f;
     static inline float sMinRPM = 1000.0f;
     static inline float sMaxRPM = 6000.0f;
     static inline float sInertia = 0.5f;
     static inline float sAngularDamping = 0.2f;
+    // transmission
     static inline float sSwitchTime = 0.5f;
     static inline float sClutchReleaseTime = 0.3f;
     static inline float sSwitchLatency = 0.5f;
     static inline float sShiftUpRPM = 4000.0f;
     static inline float sShiftDownRPM = 2000.0f;
     static inline float sClutchStrength = 10.0f;
+    // other
     static inline float sFrontCasterAngle = 0.0f;
     static inline float sFrontKingPinAngle = 0.0f;
     static inline float sFrontCamber = 0.0f;
